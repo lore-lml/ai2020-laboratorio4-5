045510929224136b02a82ce6e7fb54e1a1634342
Lorenzo Limoli <lorenzo.limoli@outlook.it>
2020-05-15 16:58:38 +0200

    Table students

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {MatSidenav} from '@angular/material/sidenav';
+import {Student} from './student.model';
+
+const STUDENTS: Array<Student> = [
+  new Student('s1', 'Lorenzo', 'Limoli'),
+  new Student('s2', 'Stefano', 'Loscalzo'),
+  new Student('s3', 'Angelo', 'Floridia')
+];
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'lab4';
+  @ViewChild(MatSidenav)
+  sidenav: MatSidenav;
+  studentTable: StudentTable = new StudentTable();
+  toggleForMenuClick() {
+    this.sidenav.toggle();
+  }
+}
+
+export class StudentTable{
+  students: Array<Student> = STUDENTS;
+  displayedColumns: string[] = ['Select', 'Id', 'FirstName', 'LastName'];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatTableModule} from '@angular/material/table';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatTabsModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatTableModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
